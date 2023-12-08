bfdd6486873fb51fbf0209bddf8a36623e09abea
kaklotar hardik <129932278+hardikkaklotar@users.noreply.github.com>
2023-12-08 18:16:36 +0530

    skills section and aos animation add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import * as Aos from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hardik-resume';
+  ngOnInit() {
+    Aos.init();
+  }
 }
